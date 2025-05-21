5c4297478ceffe38dbe97231e0999a81c604ec06
Lucianoi9 <78362707+Lucianoi9@users.noreply.github.com>
2025-05-21 01:52:32 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
       <h1>🎧 StorePlay AI Player</h1>
       <p>Ouça agora a rádio da sua loja!</p>
       <audio controls autoPlay style={{ width: '80%' }}>
-        <source src="http://3.90.14.131/station/4" type="audio/mpeg" />
+        <source src="http://3.90.14.131/public/radio_okeo" type="audio/mpeg" />
         Seu navegador não suporta áudio embutido.
       </audio>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
